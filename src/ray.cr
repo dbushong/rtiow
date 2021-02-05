39339bed70f9edba9447ca08508dbff84d6509cf
David Bushong <david@transposit.com>
2021-02-04 20:46:29 -0800

    2.1

--- src/ray.cr
+++ src/ray.cr
@@ -1,9 +1,9 @@
 require "./vec3"
 
 struct Ray
-  property origin, direction
+  getter origin, direction, tm
 
-  def initialize(@origin : Vec3, @direction : Vec3)
+  def initialize(@origin : Vec3, @direction : Vec3, @tm = 0.0)
   end
 
   def at(t : Float64)
